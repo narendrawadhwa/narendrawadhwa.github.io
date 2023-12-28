67b727a4f5c30dfe49db3afa2e7aee4efa895b8b
narendrawadhwa <bakemeawish1201@gmail.com>
2023-12-28 23:12:17 +0530

    main jsx changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const App = require('./App.jsx');
-require('./index.css');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  React.createElement(React.StrictMode, null, React.createElement(App, null))
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
