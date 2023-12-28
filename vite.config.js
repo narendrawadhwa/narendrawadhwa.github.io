6ce7d475d20cce38aef6279729411cd62c0aacdf
narendrawadhwa <bakemeawish1201@gmail.com>
2023-12-28 22:08:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb'],
+  assetsInclude: ['**/*.glb', '**/*.jsx'],
   base: '/3D_Portfolio_App/',
 
 })
