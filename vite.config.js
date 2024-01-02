dcc20b6346cb38ccf9f639249a5dae720efdfd83
narendrawadhwa <bakemeawish1201@gmail.com>
2024-01-02 23:12:46 +0530

    jsx to js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb', '**/*.jsx'],
-  // base: '/3D_Portfolio_App/',
+  assetsInclude: ['**/*.glb', '**/*.jsx', '**/*.js'],
+  base: '/3D_Portfolio_App/',
   outDir: 'dist',
+  esbuild: {
+    jsxFactory: 'React.createElement',
+    jsxFragment: 'React.Fragment',
+  },
 
 })
