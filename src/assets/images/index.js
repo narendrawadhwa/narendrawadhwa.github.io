9f7de221f1762de0dfe39229958a5175dfaa9c45
narendrawadhwa <bakemeawish1201@gmail.com>
2024-01-10 00:40:38 +0530

    deleted unwanted gif

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,8 +2,6 @@
 import logo from './logo.svg'
 import codsoft from './codsoft.png'
 import inderdeep from './inderdeep.png'
-// import arrowleft from './arrow_left.png'
-// import arrowright from './arrow_right.png';
 import arrows from './arrows.gif';
 import wavinghand from './waving_hand.gif';
 import clickLink from './click_link.gif' 
@@ -13,8 +11,6 @@ export {
     logo,
     codsoft,
     inderdeep,
-    // arrowleft,
-    // arrowright,
     arrows,
     clickLink,
     wavinghand,
